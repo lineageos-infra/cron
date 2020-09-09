3ec31cb82a703e7558b006f53189040834482f17
Tom Powell <tom@powell.io>
2020-09-08 21:26:17 -0700

    Also send reset groups to slack

--- gerrit/group_ownership.py
+++ gerrit/group_ownership.py
@@ -1,4 +1,4 @@
-from lib import rest
+from lib import rest, send_slack
 
 projects = [x for x in rest.get("/projects/?p=PROJECT").keys()]
 projects.extend([x for x in rest.get("/projects/?p=OEM").keys()])
@@ -7,8 +7,13 @@ filtered_groups = [x for x in groups.keys() if any([x.startswith("OEM"), x.start
 
 parent_id = groups['Head Developers']['id']
 
+modified = []
 for group in filtered_groups:
     _id = groups[group]['id']
     if groups[group]['owner'] != 'Head Developers':
         print(f'{group} isn\'t owned by Head Developers, fixing')
         rest.put('/groups/{}/owner'.format(_id), {'owner': parent_id})
+        modified += group
+
+if modified:
+    send_slack(f"Reset group owner on {', '.join(modified)}")
