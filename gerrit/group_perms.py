f3c802703aa9c33bfa876c98ec39d91c9441d0de
Tom Powell <tom@powell.io>
2020-09-08 20:11:09 -0700

    more lib

--- gerrit/group_perms.py
+++ gerrit/group_perms.py
@@ -2,35 +2,10 @@ import json
 
 import requests
 from requests.auth import HTTPBasicAuth
-from config import USERNAME, PASSWORD, URL
+from config import URL
 from datadiff import diff
-from lib import dprint
+from lib import dprint, rest
 
-
-class rest:
-    auth = HTTPBasicAuth(USERNAME, PASSWORD)
-
-    @classmethod
-    def get(cls, url):
-        req = requests.get(f"{URL}a{url}", auth=cls.auth)
-        dprint(req.status_code, req.text)
-        if req.status_code == 200:
-            raw = req.text[5:]
-            return json.loads(raw)
-
-    @classmethod
-    def post(cls, url, j):
-        req = requests.post(f"{URL}a{url}", auth=cls.auth, json=j)
-        dprint(req.status_code, req.text)
-        if req.status_code == 500:
-            print(f"Error applying {url}: {req.text}")
-
-    @classmethod
-    def put(cls, url, j=None):
-        req = requests.put(f"{URL}a{url}", auth=cls.auth, json=j)
-        dprint(req.status_code, req.text)
-        if req.status_code == 500:
-            print(f"Error applying {url}: {req.text}")
 
 projects = [x for x in rest.get("/projects/?p=PROJECT").keys()]
 projects.extend([x for x in rest.get("/projects/?p=OEM").keys()])
--- gerrit/lib.py
+++ gerrit/lib.py
@@ -1,5 +1,33 @@
 import os
+from config import USERNAME, PASSWORD
+
+print(f"Logging at {LOG_LEVEL}")
 
 def dprint(*args):
     if os.environ.get("LOG_LEVEL", "INFO") == "DEBUG":
         print(*args)
+
+class rest:
+    auth = HTTPBasicAuth(USERNAME, PASSWORD)
+
+    @classmethod
+    def get(cls, url):
+        req = requests.get(f"{URL}a{url}", auth=cls.auth)
+        dprint(req.status_code, req.text)
+        if req.status_code == 200:
+            raw = req.text[5:]
+            return json.loads(raw)
+
+    @classmethod
+    def post(cls, url, j):
+        req = requests.post(f"{URL}a{url}", auth=cls.auth, json=j)
+        dprint(req.status_code, req.text)
+        if req.status_code == 500:
+            print(f"Error applying {url}: {req.text}")
+
+    @classmethod
+    def put(cls, url, j=None):
+        req = requests.put(f"{URL}a{url}", auth=cls.auth, json=j)
+        dprint(req.status_code, req.text)
+        if req.status_code == 500:
+            print(f"Error applying {url}: {req.text}")
